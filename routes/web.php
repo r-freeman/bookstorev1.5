85c257074f3d9b89629e17b9848825d7cb4d9805
Ryan Freeman <info@ryanfreeman.io>
2019-10-22 21:42:13 +0100

    Added book routes

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/home', 'Admin\HomeController@index')->name('admin.home');
 Route::get('/user/home', 'User\HomeController@index')->name('user.home');
 
+Route::get('/admin/books', 'Admin\BookController@index')->name('admin.books.index');
+Route::get('/admin/books/create', 'Admin\BookController@create')->name('admin.books.create');
+Route::get('/admin/books/{id}', 'Admin\BookController@show')->name('admin.books.show');
+Route::post('/admin/books/store', 'Admin\BookController@store')->name('admin.books.store');
+Route::get('/admin/books/{id}/edit', 'Admin\BookController@edit')->name('admin.books.edit');
+Route::put('/admin/books/{id}', 'Admin\BookController@update')->name('admin.books.update');
+Route::delete('/admin/books/{id}', 'Admin\BookController@destroy')->name('admin.books.destroy');
+
+Route::get('/user/books', 'User\BookController@index')->name('user.books.index');
+Route::get('/user/books/{id}', 'User\BookController@show')->name('user.books.show');
 
